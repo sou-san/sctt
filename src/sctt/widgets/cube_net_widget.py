5a15d892673e45a0a2d2a5de07701b525e78aa58
sou-san <sou887867@gmail.com>
2024-11-13 18:25:34 +0900

    Fix src/sctt/widgets/cube_net_widget.py

--- src/sctt/widgets/cube_net_widget.py
+++ src/sctt/widgets/cube_net_widget.py
@@ -3,7 +3,7 @@ from textual.reactive import reactive
 from textual.widget import Widget
 
 from sctt.modules.simulate import Cube
-from sctt.renderables.cube_display import CubeDisplay
+from sctt.renderables.cube_display import CubeDisplay, StickerSize
 
 
 class CubeNetWidget(Widget):
@@ -16,7 +16,7 @@ class CubeNetWidget(Widget):
     def watch_scramble(self) -> None:
         cube = Cube()
         cube.apply_scramble(self.scramble)
-        self.cube_net = CubeDisplay(cube, "filled").generate_renderable_cube_net()
+        self.cube_net = CubeDisplay(cube, StickerSize.NORMAL).generate_renderable_cube_net()
 
     def render(self) -> RenderResult:
         return self.cube_net
