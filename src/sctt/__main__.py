aa2e4bde0fe18479eddbeb093b32729905d2143e
sou-san <sou887867@gmail.com>
2024-12-19 02:24:01 +0900

    Implement solve saving and stats feature

--- src/sctt/__main__.py
+++ src/sctt/__main__.py
@@ -1,8 +1,11 @@
 from sctt.app import Sctt
+from sctt.locations import get_database_file
+from sctt.modules.database import Database
 
 
 def main() -> None:
-    Sctt().run()
+    db: Database = Database(get_database_file())
+    Sctt(db).run()
 
 
 if __name__ == "__main__":
--- src/sctt/app.py
+++ src/sctt/app.py
@@ -4,16 +4,33 @@ import keyboard
 from textual import on
 from textual.app import App, ComposeResult
 from textual.binding import Binding
-from textual.containers import Vertical
+from textual.containers import Horizontal, Vertical
 from textual.events import Resize
 from textual.widgets import Footer
 
+from sctt.locations import get_cache_file
+from sctt.modules.database import Database
 from sctt.screens.blocking_screen import MIN_HEIGHT, MIN_WIDTH, BlockingScreen
 from sctt.widgets.cube_net_widget import CubeNetWidget
 from sctt.widgets.scramble_widget import ScrambleWidget
-
-# from sctt.widgets.status_widget import StatusWidgets
+from sctt.widgets.stats_widget import StatsWidget
 from sctt.widgets.timer_widget import TimerWidget
+
+
+def get_last_session_id() -> int:
+    with open(get_cache_file(), "r") as f:
+        return int(f.readline())
+
+
+def save_last_session_id(session_id: int) -> None:
+    with open(get_cache_file(), "w") as f:
+        f.write(str(session_id))
+
+
+class SolveBuffer:
+    time: float
+    scramble: str
+    session_id: int
 
 
 class AppBody(Vertical):
@@ -28,17 +45,39 @@ class Sctt(App[None]):
         Binding("space", "", "Start / Stop"),  # Dummy key binding for the keyboard lib.
     ]
 
+    def __init__(self, db: Database) -> None:
+        super().__init__()
+        self.db: Database = db
+        self.solve_buffer: SolveBuffer = SolveBuffer()
+
+        if not self.db.get_all_sessions():
+            session_id: int | None = self.db.create_session("session 1")
+
+            if session_id is None:
+                raise ValueError("Failed to create session.")
+            else:
+                save_last_session_id(session_id)
+        else:
+            session_id = get_last_session_id()
+
+        self.solve_buffer.session_id = session_id
+
     def compose(self) -> ComposeResult:
-        # with Horizontal():
-        # バックエンドの処理などがまだできていないため、コメントアウトしている。
-        # yield StatusWidgets()
         with AppBody():
-            yield ScrambleWidget()
-            self.timer_widget = TimerWidget()
-            yield self.timer_widget
-            self.cube_net_widget = CubeNetWidget()
-            yield self.cube_net_widget
+            with Horizontal():
+                yield StatsWidget()
+                with Vertical():
+                    yield ScrambleWidget()
+                    self.timer_widget = TimerWidget()
+                    yield self.timer_widget
+                    self.cube_net_widget = CubeNetWidget()
+                    yield self.cube_net_widget
             yield Footer()
+
+    def on_mount(self) -> None:
+        self.query_one(StatsWidget).update(
+            self.db.get_solve_ids_and_times(self.solve_buffer.session_id)
+        )
 
     def on_app_focus(self) -> None:
         keyboard.hook(self.timer_widget.key_events)
@@ -54,10 +93,37 @@ class Sctt(App[None]):
     def update_scramble(self) -> None:
         self.query_one(ScrambleWidget).update()
 
+    def save_solve(self) -> int | None:
+        solve_id: int | None = self.db.add_solve(
+            self.solve_buffer.time, self.solve_buffer.scramble, self.solve_buffer.session_id
+        )
+
+        return solve_id
+
+    @on(TimerWidget.Solved)
+    def update_record(self, message: TimerWidget.Solved) -> None:
+        self.solve_buffer.time = message.time_
+        solve_id: int | None = self.save_solve()
+
+        if solve_id is None:
+            raise ValueError("Failed to add solve.")
+        else:
+            self.query_one(StatsWidget).update(
+                self.db.get_solve_ids_and_times(self.solve_buffer.session_id)
+            )
+
+        save_last_session_id(self.solve_buffer.session_id)
+
+        # reset
+        self.solve_buffer = SolveBuffer()
+        self.solve_buffer.session_id = get_last_session_id()
+
     @on(ScrambleWidget.Changed)
     def update_cube_net(self, message: ScrambleWidget.Changed) -> None:
         try:
             self.cube_net_widget.update_cube_net(message.scramble, message.cube_size)
+
+            self.solve_buffer.scramble = message.scramble
         except ValueError:
             self.notify("[#ff0000][b]Error[/][/]\nInvalid scramble", severity="error")
             self.query_one(ScrambleWidget).set_inputted_scramble(None)
