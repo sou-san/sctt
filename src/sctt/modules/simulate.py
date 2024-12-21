f8c0cfe5ddc047bbe341cb8b0f37a1a0306cea7e
sou-san <sou887867@gmail.com>
2024-12-21 11:08:36 +0900

    Remove test

--- src/sctt/modules/simulate.py
+++ src/sctt/modules/simulate.py
@@ -308,22 +308,3 @@ class Cube:
                     raise ValueError(f"Invalid move: {move}")
                 else:
                     moves_map[m](num_layers)
-
-
-# test
-if __name__ == "__main__":
-    scramble: str = "R' F2 U2 L2 F2 D2 B U2 B R2 F R2 D F L D2 U' R B R"
-    expected_faces: dict[str, list[list[str]]] = {
-        "U": [["O", "B", "Y"], ["W", "W", "R"], ["O", "G", "Y"]],
-        "L": [["W", "B", "W"], ["Y", "O", "O"], ["R", "W", "Y"]],
-        "F": [["B", "W", "G"], ["W", "G", "Y"], ["R", "R", "R"]],
-        "R": [["O", "G", "O"], ["R", "R", "O"], ["G", "O", "R"]],
-        "B": [["B", "Y", "G"], ["G", "B", "O"], ["Y", "Y", "B"]],
-        "D": [["B", "B", "W"], ["R", "Y", "B"], ["W", "G", "G"]],
-    }
-
-    cube: Cube = Cube()
-    cube.apply_scramble(scramble)
-
-    assert cube.faces == expected_faces, "Test failed. Faces do not match expected state."
-    print("Passed the test.")
