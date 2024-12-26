8951a4ea245827f05cf3d82cdf7b160c113ca3e4
sou-san <sou887867@gmail.com>
2024-12-27 08:01:27 +0900

    Add key bindings to MyVerticalScroll()

--- src/sctt/widgets/my_vertical_scroll.py
+++ src/sctt/widgets/my_vertical_scroll.py
@@ -10,8 +10,8 @@ class MyVerticalScroll(VerticalScroll):
         Binding("right,l", "scroll_right", "Scroll Right", show=False),
         Binding("home,g", "scroll_home", "Scroll Home", show=False),
         Binding("end,G", "scroll_end", "Scroll End", show=False),
-        Binding("pageup,b", "page_up", "Page Up", show=False),
-        Binding("pagedown,f", "page_down", "Page Down", show=False),
+        Binding("pageup,ctrl+b", "page_up", "Page Up", show=False),
+        Binding("pagedown,ctrl+f", "page_down", "Page Down", show=False),
         Binding("ctrl+pageup", "page_left", "Page Left", show=False),
         Binding("ctrl+pagedown", "page_right", "Page Right", show=False),
     ]
