6c70c57a3bc4a02b8c4084eb2e77eb7ea2c90e36
sou-san <sou887867@gmail.com>
2024-12-27 16:11:59 +0900

    Add src/sctt/screens/dialog_screen.py

---
+++ src/sctt/screens/dialog_screen.py
@@ -0,0 +1,30 @@
+from textual import on
+from textual.app import ComposeResult
+from textual.containers import Center, Horizontal, Vertical
+from textual.screen import ModalScreen
+from textual.widgets import Button, Label
+
+
+class DialogScreen(ModalScreen[str]):
+    def __init__(self, text: str) -> None:
+        super().__init__()
+        self.text: str = text
+
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            with Center():
+                yield Label(self.text)
+            with Center():
+                with Horizontal():
+                    yield Button("Cancel", id="cancel")
+                    yield Button("Ok", id="ok")
+
+    @on(Button.Pressed)
+    def handle_button_pressed(self, event: Button.Pressed) -> None:
+        match event.button.id:
+            case "cancel":
+                self.dismiss("cancel")
+            case "ok":
+                self.dismiss("ok")
+            case _:
+                pass
